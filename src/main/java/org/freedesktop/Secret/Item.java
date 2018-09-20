2ae6df8a0e5820a57eab7bc7a301bc124ee07bb9
Ralph Plawetzki <ralph@purejava.org>
2018-09-20 10:35:32 +0200

    Add all required interfaces.

---
+++ src/main/java/org/freedesktop/Secret/Item.java
@@ -0,0 +1,10 @@
+package org.freedesktop.Secret;
+import org.freedesktop.dbus.DBusInterface;
+public interface Item extends DBusInterface
+{
+
+  public DBusInterface Delete();
+  public Struct3 GetSecret(DBusInterface session);
+  public void SetSecret(Struct4 secret);
+
+}

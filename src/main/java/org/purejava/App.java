2475a4d2f0c935e3fe46ace8743d15d4f57dccf6
Ralph Plawetzki <ralph@purejava.org>
2020-11-05 18:30:25 +0100

    Initial commit

---
+++ src/main/java/org/purejava/App.java
@@ -0,0 +1,28 @@
+package org.purejava;
+
+import org.freedesktop.dbus.connections.impl.DBusConnection;
+import org.freedesktop.dbus.exceptions.DBusException;
+import org.kde.KWallet;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) {
+        DBusConnection connection = null;
+
+        try {
+            connection = DBusConnection.getConnection(DBusConnection.DBusBusType.SESSION);
+
+            KDEWallet service = new KDEWallet(connection);
+            int handle = service.getSignalHandler().getLastHandledSignal(KWallet.walletAsyncOpened.class, "org.kde.kwalletd5").handle;
+
+        } catch (DBusException e) {
+            System.out.println(e.toString() + e.getCause());
+        }
+        try {
+            connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
